0d79ce9162bb6a4b3e1a3e56d82345917d306073
Jackieto <jackietoyt@gmail.com>
2025-03-04 10:30:03 +0100

    day1 updated

--- day1.py
+++ day1.py
@@ -1,13 +1,59 @@
 
 input = open("./inputs/day1")
 
-#Assuming theres only 2 digits per line
-total = 0
-numbers = []
 
+
+def w2n(number):
+    words = [ "zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten"]
+    for i in words:
+        number = number.replace(i, str(words.index(i)))
+    return number
+
+
+def computeNumber(tuples):
+    
+    #first number should be first min item sorted by index, second number max item 
+    
+    
+    first_tuple = min(tuples,key=lambda x: x[1])
+    first = w2n(first_tuple[0])
+    
+    
+    second_tuple = max(tuples,key=lambda x: x[1])
+    second = w2n(second_tuple[0])
+    
+    number = first + second
+    #print("Numero: ",number)
+    return int(number)    
+    
+
+numbers = ["1","2","3","4","5","6","7","8","9","one","two","three","four","five","six","seven","eight","nine"]
+
+total = 0
+
+for line in input:
+    
+    tuples = []
+    
+    for number in numbers:
+        index = line.find(number)
+        if(index != -1):
+            number_index = (number, index)
+            tuples.append(number_index)
+        
+    number = computeNumber(tuples)
+    total += number
+    
+      
+print(total)
+
+
+"""PART ONE
 for line in input:
     number = ""
     count = 0
+    
+    index = 0
     
     first = ""
     second = ""
@@ -18,13 +64,17 @@ for line in input:
             first = char
             number += first
             count += 1
+            index += 1
             continue
             
         
         #second digit
         if char.isdigit() and count != 0:
           # print("Second: ",char)
-            second = char         
+            second = char
+            index += 1
+            continue
+        index += 1         
         
     
     #If theres only 1 digit in line it repeats
@@ -43,3 +93,4 @@ for n in numbers:
 
     
 print(total)
+"""
